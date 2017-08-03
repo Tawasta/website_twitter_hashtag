8c58aff674e25242ed937146df909c12208be6a2
Timo Talvitie <timo.talvitie@vizucom.com>
2017-08-04 00:03:49 +0300

    Add button for deleting old tweets from DB

--- models/website_config_settings.py
+++ models/website_config_settings.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class WebsiteConfigSettings(models.TransientModel):
@@ -7,6 +7,12 @@ class WebsiteConfigSettings(models.TransientModel):
     _inherit = 'website.config.settings'
 
     _TWITTER_SEARCH_MODES = [('favorites', 'Favorites'), ('hashtag', 'By Hashtag')]
+
+    @api.multi
+    def purge_tweets(self):
+        self.ensure_one()
+        self.env['website.twitter.tweet'].search(args=[]).unlink()
+
 
     twitter_search_mode = fields.Selection(_TWITTER_SEARCH_MODES, related='website_id.twitter_search_mode', string='Twitter Search Mode', default='favorites')
     twitter_hashtag = fields.Char(related='website_id.twitter_hashtag', string='Hashtag', help='''Hashtag to search''')
